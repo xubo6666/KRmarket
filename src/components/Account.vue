9db79ecec4fe20b07bdf6049b136faba7f25523f
hsu <hsu@mail.nblqtech.com>
2023-07-27 00:56:16 +0800

    WIP

---
+++ src/components/Account.vue
@@ -0,0 +1,204 @@
+<template>
+  <v-container>
+    <v-card
+      class="mx-auto mt-5"
+      max-width="375"
+      min-height="100"
+      rounded="xl"
+      style="
+        padding: -10;
+        background-image: url('b2.png');
+        background-size: cover;
+        background-position: center;
+      "
+    >
+      <v-row class="text-center mt-3 mb-3">
+        <v-col cols="12">
+          <div class="text-red">Premium Member</div>
+        </v-col>
+        <v-col cols="12" class="text-white"> 信用分數： </v-col>
+      </v-row>
+    </v-card>
+    <v-card
+      v-for="(card, index) in cardData"
+      :key="index"
+      color="indigo-darken-4 lighten-2"
+      class="mx-auto mt-5"
+      max-width="375"
+      min-height="125"
+      rounded="xl"
+    >
+      <v-card-title class="text-orange">{{ card.title }}</v-card-title>
+      <v-row class="text-center">
+        <v-col cols="4">
+          {{ card.label1 }}
+          <div>{{ card.value1 }}</div>
+        </v-col>
+        <v-col cols="4">
+          {{ card.label2 }}
+          <div>{{ card.value2 }}</div>
+        </v-col>
+        <v-col cols="4">
+          {{ card.label3 }}
+          <div>{{ card.value3 }}</div>
+        </v-col>
+      </v-row>
+    </v-card>
+    <!-- Bank card -->
+    <!-- <v-card
+
+      color="indigo-darken-4 lighten-2"
+      class="mx-auto mt-5"
+      max-width="375"
+      min-height="125"
+      rounded="xl"
+    >
+      <v-card-title class="text-orange">銀行卡</v-card-title>
+
+    </v-card> -->
+
+    <v-card
+      color="purple-darken-4
+             lighten-2"
+      class="mx-auto mt-5 mb-5"
+      max-width="550"
+      min-height="200"
+      rounded="xl"
+    >
+      <v-row class="text-center">
+        <v-col cols="4">
+          <v-btn variant="text" size="large"
+            ><v-icon
+              icon="mdi-swap-horizontal"
+              style="color: orange; font-size: 25px; position: absolute"
+            />
+            <div class="btns" style="margin-top: 20px">兌換</div>
+          </v-btn>
+        </v-col>
+        <v-col cols="4">
+          <v-btn variant="text" size="large"
+            ><v-icon
+              icon="mdi-swap-horizontal"
+              style="color: orange; font-size: 25px; position: absolute"
+            />
+            <div class="btns" style="margin-top: 20px">兌換</div>
+          </v-btn>
+        </v-col>
+        <v-col cols="4">
+          <v-btn variant="text" size="large"
+            ><v-icon
+              icon="mdi-exit-to-app"
+              style="color: orange; font-size: 25px; position: absolute"
+            />
+            <div class="btns" style="margin-top: 20px">提款</div>
+          </v-btn>
+        </v-col>
+        <v-col cols="4">
+          <v-btn variant="text" size="large"
+            ><v-icon
+              icon="mdi-cash-clock"
+              style="color: orange; font-size: 25px; position: absolute"
+            />
+            <div class="btns" style="margin-top: 20px">到期日</div>
+          </v-btn>
+        </v-col>
+        <v-col cols="4">
+          <v-btn variant="text" size="large"
+            ><v-icon
+              icon="mdi-hand-coin-outline"
+              style="color: orange; font-size: 25px; position: absolute"
+            />
+            <div class="btns" style="margin-top: 20px">貸款</div>
+          </v-btn>
+        </v-col>
+        <v-col cols="4">
+          <v-btn variant="text" size="large"
+            ><v-icon
+              icon="mdi-handshake-outline"
+              style="color: orange; font-size: 25px; position: absolute"
+            />
+            <div class="btns" style="margin-top: 20px">充值</div>
+          </v-btn>
+        </v-col>
+        <v-col cols="4">
+          <v-btn variant="text" size="large"
+            ><v-icon
+              icon="mdi-face-agent"
+              style="color: orange; font-size: 25px; position: absolute"
+            />
+            <div class="btns" style="margin-top: 20px">客服中心</div>
+          </v-btn>
+        </v-col>
+        <v-col cols="4">
+          <v-btn variant="text" size="large"
+            ><v-icon
+              icon="mdi-file-document"
+              style="color: orange; font-size: 25px; position: absolute"
+            />
+            <div class="btns" style="margin-top: 20px">資產紀錄</div>
+          </v-btn>
+        </v-col>
+        <v-col cols="4">
+          <v-btn variant="text" size="large"
+            ><v-icon
+              icon="mdi-text-box-search"
+              style="color: orange; font-size: 25px; position: absolute"
+            />
+            <div class="btns" style="margin-top: 20px">訂單記錄</div>
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-container>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      cardData: [
+        {
+          title: "錢包 USDT餘額",
+          label1: "可用數量",
+          value1: "123",
+          label2: "拍賣中",
+          value2: "123",
+          label3: "競標餘額",
+          value3: "123",
+        },
+        {
+          title: "平臺USDT餘額",
+          label1: "競標餘額",
+          value1: "123",
+          label2: "貸款總金額",
+          value2: "123",
+          label3: "USDT餘額",
+          value3: "123",
+        },
+        {
+          title: "獲利",
+          label1: "累積獲利",
+          value1: "123",
+          label2: "今日獲利",
+          value2: "123",
+          label3: "已提領USDT",
+          value3: "123",
+        },
+      ],
+    };
+  },
+};
+</script>
+<style>
+.btns {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%); /* 讓文字和圖示置中 */
+  font-size: 12px;
+  overflow: hidden;
+  overflow-wrap: anywhere;
+  display: flex;
+  font-weight: bold;
+  justify-content: center;
+}
+</style>
---
+++ src/components/product/Level.vue
@@ -0,0 +1,166 @@
+<template>
+  <v-container>
+    <v-card
+      color="indigo-darken-4
+             lighten-2"
+      class="mx-auto"
+      max-width="500"
+      max-height="mx-auto"
+      rounded="xl"
+    >
+      <v-row class="text-center mt-3 mb-3">
+        <v-col cols="6">
+          <v-card-subtitle>拍賣中</v-card-subtitle>
+          <div>123</div>
+        </v-col>
+        <v-col cols="6">
+          <v-card-subtitle>今日收益</v-card-subtitle>
+          <div>567</div>
+        </v-col>
+        <v-col cols="6">
+          <v-card-subtitle>競標可用餘額</v-card-subtitle>
+          <div>789</div>
+        </v-col>
+        <v-col cols="6">
+          <v-card-subtitle>可用餘額</v-card-subtitle>
+          <div>0123</div>
+        </v-col>
+      </v-row>
+    </v-card>
+    <v-card
+      v-for="(level, index) in levelData"
+      :key="level.id"
+      variant="outlined"
+      class="mx-auto gradient-0 gradient-1 gradient-2 gradient-3 gradient-4"
+      max-width="500"
+      max-height="mx-auto"
+      :style="{
+        margin: '50px auto',
+        border: '2px solid ' + getCardColor(index),
+        backgroundImage: `linear-gradient(to bottom right, ${getGradientColors(
+          index
+        )})`,
+        cursor: 'pointer',
+      }"
+    >
+      <v-img
+        :src="level.icon"
+        width="50"
+        height="50"
+        :aspect-ratio="1"
+        class="mt-5 mr-5 position-absolute"
+        style="top: -15px; right: -15px"
+      />
+      <v-row>
+        <v-col cols="5">
+          <v-img
+            :src="level.image"
+            :alt="level.title"
+            contain
+            :aspect-ratio="1"
+            class="mx-auto"
+            max-width="200px"
+            max-height="200px"
+          ></v-img>
+        </v-col>
+        <v-col cols="7" class="text-white">
+          <div class="pt-3">{{ level.title }}</div>
+          <div class="text-white mt-5" style="font-size: 14px">
+            1.0~2.0 USDT
+          </div>
+          <div style="white-space: nowrap">
+            點擊進入<v-icon size="12" color="#B6B6B6">mdi-chevron-right</v-icon>
+            <v-icon size="12" color="#B6B6B6">mdi-chevron-right</v-icon>
+            <v-icon size="12" color="#B6B6B6">mdi-chevron-right</v-icon>
+          </div>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-container>
+</template>
+<script>
+import { useI18n } from "vue-i18n";
+
+export default {
+  setup() {
+    const { t, locale } = useI18n();
+    return {
+      t,
+      locale,
+    };
+  },
+  data: () => ({
+    levelData: [
+      {
+        id: 1,
+        title: "Level 1",
+        description: "Beginner",
+        amount: 0.1,
+        image: "./test1.jpeg",
+        icon: "./icon.png",
+      },
+      {
+        id: 2,
+        title: "Level 2",
+        description: "Intermediate",
+        amount: 0.2,
+        image: "./test1.jpeg",
+      },
+      {
+        id: 3,
+        title: "Level 3",
+        description: "Expert",
+        amount: 0.3,
+        image: "./test1.jpeg",
+      },
+      {
+        id: 4,
+        title: "Level 4",
+        description: "Master",
+        amount: 0.4,
+        image: "./test1.jpeg",
+      },
+      {
+        id: 5,
+        title: "Level 5",
+        description: "Legend",
+        amount: 0.5,
+        image: "./test1.jpeg",
+      },
+    ],
+    getCardColor(index) {
+      switch (index) {
+        case 0:
+          return "#1e5f74";
+        case 1:
+          return "#23316d";
+        case 2:
+          return "#2a2f4f";
+        case 3:
+          return "#ffa000";
+        case 4:
+          return "#9e9e9e";
+        default:
+          return "#bdc3c7";
+      }
+    },
+    getGradientColors(index) {
+      switch (index) {
+        case 0:
+          return "#0d2147, #1e5f74";
+        case 1:
+          return "#02081f, #23316d";
+        case 2:
+          return "#03030c, #2a2f4f";
+        case 3:
+          return "#46091e, #821e3a";
+        case 4:
+          return "#601b40, #b83b5e";
+        default:
+          return "#bdc3c7";
+      }
+    },
+  }),
+};
+</script>
+<style></style>

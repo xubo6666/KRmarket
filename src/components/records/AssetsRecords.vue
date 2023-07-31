d3dbf123661d08de0c9add0cd4d4b69f70314a01
hsu <hsu@mail.nblqtech.com>
2023-08-01 00:30:30 +0800

    feat: add AssetsRecords

---
+++ src/components/records/AssetsRecords.vue
@@ -0,0 +1,82 @@
+<template>
+  <v-card
+    rounded="xl"
+    class="mx-auto my-12 card-border"
+    max-width="550"
+    color="indigo-darken-4 lighten-2"
+    style="width: 95%"
+  >
+    <v-tabs v-model="tab">
+      <v-tab value="Withdraw"> 提款 </v-tab>
+      <v-tab value="Deposit"> 充值 </v-tab>
+      <v-tab value="Order"> 訂單記錄 </v-tab>
+    </v-tabs>
+    <v-window v-model="tab">
+      <v-window-item value="Withdraw">
+        <v-table class="rounded-lg text-caption">
+          <tbody>
+            <tr>
+              <td>USDT</td>
+              <td>151</td>
+              <td class="text-grey">07-07 23:59</td>
+              <td class="text-green">成功</td>
+            </tr>
+          </tbody>
+        </v-table></v-window-item
+      >
+
+      <v-window-item value="Deposit">
+        <v-table class="rounded-lg text-caption">
+          <tbody>
+            <tr>
+              <td>USDT</td>
+              <td>151</td>
+              <td class="text-grey">07-07 23:59</td>
+              <td class="text-green">成功</td>
+            </tr>
+          </tbody>
+        </v-table></v-window-item
+      >
+      <v-window-item value="Order">
+        <v-table class="rounded-lg text-caption">
+          <tbody>
+            <tr>
+              <td>
+                <v-img src=/test.jpg alt="imgae" width="50" height="75" />
+              </td>
+              <td>100USDT</td>
+              <td class="text-grey">07-07 23:59</td>
+            </tr>
+            <tr>
+              <td>
+                <v-img src=/test.jpg alt="imgae" width="50" height="75" />
+              </td>
+              <td>100USDT</td>
+              <td class="text-grey">07-07 23:59</td>
+            </tr>
+          </tbody>
+        </v-table>
+      </v-window-item></v-window
+    >
+  </v-card>
+</template>
+<script>
+import { useI18n } from "vue-i18n";
+
+export default {
+  setup() {
+    const { t, locale } = useI18n();
+    return {
+      t,
+      locale,
+    };
+  },
+  data() {
+    return {
+      tab: "Withdraw",
+      notCompleted: [],
+      completed: [],
+    };
+  },
+};
+</script>
